0bc030de5dff46f6f8b91d9ea9dbc39f2afd4489
Geoff Deal <geoffdeal@proton.me>
2023-12-04 11:13:14 -0330

    Second Commit, Testing Fetch

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const date = Date();
 console.log(date);
 document.getElementById('timestamp').innerHTML = date;
+
+fetch('https://smbc-rss-plus.mindflakes.com/rss.xml')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error ("Problem fetching feed!");
+        }
+    })
+    .then(xmlData => {
+        const parser = new DOMParser();
+        const 
+    })
