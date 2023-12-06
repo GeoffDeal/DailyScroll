f8f24ced6a576be25fd9b076442a7760cecbcf90
Geoff Deal <geoffdeal@proton.me>
2023-12-06 14:23:47 -0330

    Cleaned up html, first pass on collapsible

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+// Get current date and time
 const date = Date();
 console.log(date);
 document.getElementById('timestamp').innerHTML = date;
 
-fetch('https://smbc-rss-plus.mindflakes.com/rss.xml')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error ("Problem fetching feed!");
+
+// Collapsilbe buttons
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var contentFeed = this.nextElementSibling;
+        var contentFeedStyle = window.getComputedStyle(contentFeed);
+        if (contentFeedStyle.style.display === "block" || contentFeedStyle.display === "") {
+            contentFeed.style.display = "none";
         }
-    })
-    // .then(xmlData => {
-    //     const parser = new DOMParser();
-    //     const 
-    // })
+            else{
+                contentFeed.style.display = "block";
+            }
+    });
+};
