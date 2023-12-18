1030435a70fdeb154bfbaf1f95d6ccd719842a91
Geoff Deal <geoffdeal@proton.me>
2023-12-17 22:44:13 -0330

    Cleaned up code, content cards

--- index.js
+++ index.js
@@ -5,25 +5,6 @@ const date =  unformatedDate.toLocaleString();
 document.getElementById('timestamp').innerHTML = date;
 
 
-// Collapsible buttons
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var contentFeed = this.nextElementSibling;
-        var contentFeedStyle = window.getComputedStyle(contentFeed);
-        if (contentFeedStyle.display === "block" || contentFeedStyle.display === "") {
-            contentFeed.style.display = "none";
-        }   else{
-                contentFeed.style.display = "block";
-            }
-        
-        this.parentNode.classList.toggle("active");
-    });
-};
 
 // Collapsible sidebar
 
