2ad6a65fa3a5ceb822df576abeb53c71adf0aa4c
Geoff Deal <geoffdeal@proton.me>
2023-12-22 16:01:23 -0330

    Feed Form Folder List, Tab Functions Start

--- index.js
+++ index.js
@@ -7,7 +7,26 @@ function sideBar() {
 
 document.getElementById("sidebarButton").onclick = sideBar;
 
-//Popup Forms
+// Tab Switching Display
+
+function tabDisplay(whichTab) {
+
+    let feedContent = document.getElementsByClassName("contentFeed");
+    for (let i = 0; feedContent.length; i++) {
+        feedContent[i].classList.remove("contentDisplay");
+    }
+    let tabList = document.getElementsByClassName("tabGroup");
+    for (let i = 0; tabList.length; i++) {
+        tabList[i].classList.remove("active");
+    }
+
+    document.getElementById(whichTab).classList.add("contentDisplay");
+    document.getElementById("tab" + whichTab).classList.add("active");
+}
+
+
+
+// Popup Forms and Form Folder List
 
 function openForm (x) {
     document.getElementById(x).style.display = "block";
@@ -17,10 +36,33 @@ function closeForm (x) {
     document.getElementById(x).style.display = "none";
 };
 
+const folderList = ["All", "Weather", "News", "Comics"];
+
+for (let i = 1; i < folderList.length; i++) {
+    let radioContainer = document.getElementById("folderRadios");
+
+    let radio = document.createElement("input");
+    radio.type = "radio";
+    radio.id = "radio" + folderList[i];
+    radio.name = "folderChoice";
+    radio.value = folderList[i];
+    radioContainer.appendChild(radio);
+
+    let label = document.createElement("label");
+    label.htmlFor = "radio" + folderList[i];
+    label.innerHTML = folderList[i];
+    radioContainer.appendChild(label);
+
+    let lineBreak = document.createElement("br");
+    radioContainer.appendChild(lineBreak);
+}
+
 // Creating new folders
 
 function createFolder(){
     let folderName = document.getElementById('nfolder').value;
+
+    folderList.push(folderName);
 
     let newFolder = document.createElement('div');
     newFolder.className = 'contentFeed';
@@ -30,6 +72,7 @@ function createFolder(){
 
     let newTab = document.createElement('button');
     newTab.className = 'folderTab';
+    newTab.id = 'tab' + folderName;
     newTab.innerHTML = folderName;
 
     document.getElementById('folderTabs').appendChild(newTab);
